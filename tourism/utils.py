6eafd014de410e3e7f3a87f853e92eeb57cbe63c
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-04-15 15:47:22 +0530

    some changes in utils files

--- tourism/utils.py
+++ tourism/utils.py
@@ -4,29 +4,53 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
-
+import yaml
+from typing import Dict, Tuple
 from tourism.exception import TourismException
 from tourism.logger import logging
 
-def save_object(file_path, obj):
-    try:
-        dir_path = os.path.dirname(file_path)
 
-        os.makedirs(dir_path, exist_ok=True)
+SCHEMA_CONFIG_FILE = "config/schema.yaml"
 
-        with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+class MainUtils:
+    
+    @staticmethod
+    def save_object(file_path, obj):
+        try:
+            dir_path = os.path.dirname(file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            with open(file_path, "wb") as file_obj:
+                dill.dump(obj, file_obj)
 
-    except Exception as e:
-        logging.info('Exception Occured in save_object function utils')
-        raise TourismException(e, sys)
+        except Exception as e:
+            logging.info('Exception Occured in save_object function utils')
+            raise TourismException(e, sys)
+    
+    @staticmethod
+    def load_object(file_path):
+        try:
+            with open(file_path, "rb") as file_obj:
+                return dill.load(file_obj)
+        except Exception as e:
+            logging.info('Exception Occured in load_object function utils')
+            raise TourismException(e, sys)
 
-def load_object(file_path):
-    try:
-        with open(file_path, "rb") as file_obj:
-            return dill.load(file_obj)
-    except Exception as e:
-        logging.info('Exception Occured in load_object function utils')
-        raise TourismException(e, sys)
+
+    def read_yaml_file(self, filename: str) -> dict:
+        try:
+            with open(filename, "rb") as yaml_file:
+                return yaml.safe_load(yaml_file)
+
+        except Exception as e:
+            raise TourismException(e, sys) from e
 
 
+    def read_schema_config_file(self) -> dict:
+        try:
+            schema_config = self.read_yaml_file(SCHEMA_CONFIG_FILE)
+
+            return schema_config
+        except Exception as e:
+            raise TourismException(e, sys)
+
+        
